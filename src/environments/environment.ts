3665966c0cf85d9bbc0619db8dc3f57e6bf76f93
Benjamin Egers <b.egers@unasat.sr>
2023-03-31 17:42:33 -0300

    Added new api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api/v1/messages'
+  apiBaseUrl: 'https://encrypt-backend-production.up.railway.app/api/v1/messages'
 };
 
 /*
