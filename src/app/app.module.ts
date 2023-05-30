c580dbe93a4b95b0841584ab10e35eef075e5dab
Benjamin Egers <b.egers@unasat.sr>
2023-05-30 10:38:42 -0300

    Removed unness files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,16 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { AddMessageComponent } from './component/add-message/add-message.component';
 import { CryptButtonComponent } from './component/crypt-button/crypt-button.component';
-import { TextAreaComponent } from './component/text-area/text-area.component';
 import { FindMessageComponent } from './component/find-message/find-message.component';
 import { IntroCardsComponent } from './component/intro-cards/intro-cards.component';
 import { DisclaimerOptionsComponent } from './component/disclaimer-options/disclaimer-options.component';
 import { KeyOptionsComponent } from './component/key-options/key-options.component';
-import { MessageViewComponent } from './component/message-view/message-view.component';
-import { SearchMessageComponent } from './component/search-message/search-message.component';
 import { SafeInfoComponent } from './component/safe-info/safe-info.component';
 import { HomeComponent } from './component/home/home.component';
 import { EncryptPageComponent } from './component/encrypt-page/encrypt-page.component';
 import { DecryptPageComponent } from './component/decrypt-page/decrypt-page.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { FooterComponent } from './component/footer/footer.component';
-import { PrimaryBtnComponent } from './component/primary-btn/primary-btn.component';
-import { OneKeyFormComponent } from './component/one-key-form/one-key-form.component';
-import { TwoKeyFormComponent } from './component/two-key-form/two-key-form.component';
-import { ThreeKeyFormComponent } from './component/three-key-form/three-key-form.component';
 
 
 const appRoutes: Routes = [
@@ -39,23 +32,16 @@ const appRoutes: Routes = [
     AppComponent,
     AddMessageComponent,
     CryptButtonComponent,
-    TextAreaComponent,
     FindMessageComponent,
     IntroCardsComponent,
     DisclaimerOptionsComponent,
     KeyOptionsComponent,
-    MessageViewComponent,
-    SearchMessageComponent,
     SafeInfoComponent,
     HomeComponent,
     EncryptPageComponent,
     DecryptPageComponent,
     NavbarComponent,
-    FooterComponent,
-    PrimaryBtnComponent,
-    OneKeyFormComponent,
-    TwoKeyFormComponent,
-    ThreeKeyFormComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
